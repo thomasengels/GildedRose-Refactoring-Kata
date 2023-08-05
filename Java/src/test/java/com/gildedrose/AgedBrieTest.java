8d70d802efa48e11f54ddd17a0b242bff3ba594b
Thomas Engels <thomas.engels92@gmail.com>
2023-08-05 18:24:49 +0200

    structure tests with given when then

--- Java/src/test/java/com/gildedrose/AgedBrieTest.java
+++ Java/src/test/java/com/gildedrose/AgedBrieTest.java
@@ -7,10 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AgedBrieTest {
    @Test
    void qualityIncreasesWhenSellInDecreases(){
+       //given
        AgedBrie agedBrie = new AgedBrie("brie", 10, 10);
        assertEquals(10, agedBrie.sellIn);
        assertEquals(10, agedBrie.quality);
+
+       //when
        agedBrie.updateQuality();
+
+       //then
        assertEquals(9, agedBrie.sellIn);
        assertEquals(11, agedBrie.quality);
    }
--- Java/src/test/java/com/gildedrose/SulfurasTest.java
+++ Java/src/test/java/com/gildedrose/SulfurasTest.java
@@ -7,10 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SulfurasTest {
     @Test
     void qualityIsAlways80(){
+        //given
         Sulfuras sulfuras = new Sulfuras("sulfuras", 10);
         assertEquals(80, sulfuras.quality);
         assertEquals(10, sulfuras.sellIn);
+
+        //when
         sulfuras.updateQuality();
+
+        //then
         assertEquals(80, sulfuras.quality);
         assertEquals(9, sulfuras.sellIn);
     }
