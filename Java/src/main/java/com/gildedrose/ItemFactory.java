b9e463a89291e9f2079c400c78432b81b034a224
Thomas Engels <thomas.engels92@gmail.com>
2023-08-05 19:13:24 +0200

    implement all item variants in factory builder

--- Java/src/main/java/com/gildedrose/ItemFactory.java
+++ Java/src/main/java/com/gildedrose/ItemFactory.java
@@ -2,6 +2,18 @@ package com.gildedrose;
 
 public class ItemFactory {
     Item buildItem(String name, int sellIn, int quality){
+        if(name.toLowerCase().contains("conjured")){
+            return new Conjured(name, sellIn, quality);
+        }
+        else if(name.toLowerCase().contains("backstagepass")){
+            return new BackStagePass(name, sellIn, quality);
+        }
+        else if (name.toLowerCase().contains("agedbrie")){
+            return new AgedBrie(name, sellIn, quality);
+        }
+        else if(name.toLowerCase().contains("sulfuras")){
+            return new Sulfuras(name, sellIn);
+        }
         return new Item(name, sellIn, quality);
     }
 }
