0162fbf04549c6c07936c1adda03a7aef1e8a7eb
Thomas Engels <thomas.engels92@gmail.com>
2023-08-05 18:18:30 +0200

    quality increases by factor 2 when sellIn is 10 or lower

--- Java/src/test/java/com/gildedrose/BackStagePassTest.java
+++ Java/src/test/java/com/gildedrose/BackStagePassTest.java
@@ -19,4 +19,23 @@ class BackStagePassTest {
         assertEquals(11, pass.sellIn);
         assertEquals(11, pass.quality);
     }
+
+    @Test
+    void whenSellInIsHigherThen5QualityIncreasesByFactor2(){
+        //given
+        BackStagePass pass = new BackStagePass("rammstein",11,10);
+        assertEquals(11, pass.sellIn);
+        assertEquals(10, pass.quality);
+
+        //when
+        pass.updateQuality();
+
+        //then
+        assertEquals(10, pass.sellIn);
+        assertEquals(20, pass.quality);
+
+        pass.updateQuality();
+        assertEquals(9, pass.sellIn);
+        assertEquals(40, pass.quality);
+    }
 }
