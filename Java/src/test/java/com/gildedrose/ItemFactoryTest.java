c13adb0d391acfc7dc4b84d1802549faaf19fbb3
Thomas Engels <thomas.engels92@gmail.com>
2023-08-05 19:17:50 +0200

    write tests for all item variants in ItemFactory

--- Java/src/test/java/com/gildedrose/ItemFactoryTest.java
+++ Java/src/test/java/com/gildedrose/ItemFactoryTest.java
@@ -6,6 +6,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ItemFactoryTest {
     @Test
+    void whenNameContainsAgedBrieReturnAgedBrieInstance(){
+        //given
+        String name = "AgedBrie";
+        //when
+        Item item = new ItemFactory().buildItem(name, 10,10);
+
+        //then
+        assertEquals(AgedBrie.class, item.getClass());
+    }
+
+    @Test
+    void whenNameContainsSuflurasReturnSulfurasInstance(){
+        //given
+        String name = "Sulfuras";
+        //when
+        Item item = new ItemFactory().buildItem(name, 10,10);
+
+        //then
+        assertEquals(Sulfuras.class, item.getClass());
+    }
+
+    @Test
+    void whenNameContainsBackStagePassReturnBackstagePassInstance(){
+        //given
+        String name = "Backstagepass";
+        //when
+        Item item = new ItemFactory().buildItem(name, 10,10);
+
+        //then
+        assertEquals(BackStagePass.class, item.getClass());
+    }
+
+    @Test
+    void whenNameContainsConjuredReturnConjuredInstance(){
+        //given
+        String name = "conjured";
+        //when
+        Item item = new ItemFactory().buildItem(name, 10,10);
+
+        //then
+        assertEquals(Conjured.class, item.getClass());
+    }
+
+    @Test
     void whenNameContainsAnythingReturnItemInstance(){
         //given
         String name = "cookie";
