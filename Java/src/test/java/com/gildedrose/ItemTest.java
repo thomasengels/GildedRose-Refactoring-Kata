95665250addc20d8db7ce7172aa31895ce0a7221
Thomas Engels <thomas.engels92@gmail.com>
2023-08-16 20:43:24 +0200

    verify that sellIn is decreased when update is called

---
+++ Java/src/test/java/com/gildedrose/ItemTest.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ItemTest {
+    @Test
+    void whenUpdatingItemThenSellIsIsDecreased(){
+        //when
+        Updatable updater = Mockito.mock(Updatable.class);
+        Item item = new Item(updater, 10, "item", 10);
+
+        //when
+        item.update();
+
+        //then
+        Assertions.assertEquals(9, item.sellIn);
+        Mockito.verify(updater, Mockito.times(1)).updateQuality(10, 9);
+    }
+}
